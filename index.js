f0c5ed1d6309cced0f4d39ee1806118669cae483
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-07-24 00:03:37 +0300

    x and y axes added

--- index.js
+++ index.js
@@ -1,57 +1,78 @@
-document.addEventListener("DOMContentLoaded",
-    async () => {
-        const dataUrl = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
-        let data = await fetch(dataUrl);
-        let dataset = await data.json();
-        // console.log(dataset);
+const margin = {
+    top: 40,
+    right: 20,
+    bottom: 60,
+    left: 80
+}
 
-        const margin = {
-            top: 40,
-            right: 20,
-            bottom: 60,
-            left: 80
-        }
+const svgHeight = 500,
+    svgWidth = 900;
 
-        const svgHeight = 500,
-            svgWidth = 900;
+const height = svgHeight - margin.top - margin.bottom,
+    width = svgWidth - margin.left - margin.right;
 
-        const height = svgHeight - margin.top - margin.bottom,
-            width = svgWidth - margin.el - margin.right;
+svg = d3
+    .select("#graphArea")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth)
+    .style("background", "#eaeaea")
 
-        svg = d3.select("#graphArea")
-            .append("svg")
-            .attr("height", svgHeight)
-            .attr("width", svgWidth)
-            .style("background", "#eaeaea")
+document.addEventListener("DOMContentLoaded", async () => {
+    const dataUrl = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cycli" +
+        "st-data.json";
+    let data = await fetch(dataUrl);
+    let dataset = await data.json();
+    // console.log(dataset);
 
-        let modifiedTime = dataset.map(el => {
-            timeMinutesSecondsArray = el.Time.split(":");
-            timeMinute = timeMinutesSecondsArray[0];
-            timeSecond = timeMinutesSecondsArray[1];
-            dateTime = new Date(`01/01/1970 00:${timeMinute}:${timeSecond}`)
-            return dateTime;
-        })
+    let modifiedTime = dataset.map(el => {
+        timeMinutesSecondsArray = el
+            .Time
+            .split(":");
+        timeMinute = timeMinutesSecondsArray[0];
+        timeSecond = timeMinutesSecondsArray[1];
+        dateTime = new Date(`01/01/1970 00:${timeMinute}:${timeSecond}`)
+        return dateTime;
+    })
 
-        let timeExtent = d3.extent(modifiedTime, (d) => d);
-        let yAxisScale = d3.scaleTime()
-            .domain(timeExtent).range([0, height]);
+    let timeExtent = d3.extent(modifiedTime, (d) => d);
+    let yAxisScale = d3
+        .scaleTime()
+        .domain(timeExtent)
+        .range([0, height]);
 
-            
-        let timeFormat = d3.timeFormat("%M:%S");
-        let yAxis = d3
-            .axisLeft()
-            .tickFormat(timeFormat)
-            .scale(yAxisScale);
+    let timeFormat = d3.timeFormat("%M:%S");
+    let yAxis = d3
+        .axisLeft()
+        .tickFormat(timeFormat)
+        .scale(yAxisScale);
 
-        svg
-            .append("g")
-            .call(yAxis)
-            .attr("id", "y-axis")
-            .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    svg
+        .append("g")
+        .call(yAxis)
+        .attr("id", "y-axis")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
-    }
+    const YearsInData = dataset.map(d => d.Year);
+    let dataYearMinimum = d3.min(YearsInData);
+    let dataYearMaximum = d3.max(YearsInData);
 
+    let xAxisScale = d3
+        .scaleTime()
+        .domain([dataYearMinimum-1, dataYearMaximum+2])
+        .range([0, width]);
 
+    let yearFormat = d3.format("d");
+    let xAxis = d3
+        .axisBottom()
+        .tickFormat(yearFormat)
+        .scale(xAxisScale);
 
+    const xAxisTranslateY = svgHeight - margin.bottom;
+    svg
+        .append("g")
+        .call(xAxis)
+        .attr("id", "x-axis")
+        .attr("transform", `translate(${margin.left}, ${xAxisTranslateY})`)
 
-)
+})
